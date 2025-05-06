b0de11a2bbeb7bc382713447ddc083d8453f6d1a
Barbaros İlhan <barbarosilhan@192.168.1.107>
2025-05-06 17:09:30 +0300

    ilk yükleme

---
+++ ödev.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+namespace YazilimciOl
+{
+
+    class Program
+    {
+
+        static void Main(String[] args)
+        {
+
+            string[] nedenler = {
+
+                "Daha konforlu bir yaşam için öğreniyorum.",
+                "İstediğim yerde çalışabilme özgürlüğü.",
+                "boş vakitlerimi verimli bir şey ile geçirebilmek için."
+            };
+
+            Console.WriteLine("Neden yazılımcı olmalıyım?");
+            Console.WriteLine("--------------------------");
+
+            foreach (string neden in nedenler)
+            {
+                Console.WriteLine("-" + neden);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
